93189175e03864e03090bf2217ca8963dbc3eca2
shaune-marx <shaunemarx@gmail.com>
2025-09-25 19:19:26 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,4 +33,16 @@ export const metadata: Metadata = {
   },
 };
 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={spaceMono.className} style={{ textTransform: "lowercase" }}>
+        {children}
+      </body>
+    </html>
+  );
 }

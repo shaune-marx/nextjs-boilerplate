6cb9c4532b4cbffb9248f72f06cdf7db7c9aff3a
shaune-marx <shaunemarx@gmail.com>
2025-09-29 16:53:42 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,13 @@ export const metadata: Metadata = {
   },
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  themeColor: "#111111",
+};
+
+
 export default function RootLayout({
   children,
 }: {
